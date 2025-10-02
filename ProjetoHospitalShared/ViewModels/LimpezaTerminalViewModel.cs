bf599de58eae62d650ceea2c1b28d1205d85c882
RodrigoMentz <95316880+RodrigoMentz@users.noreply.github.com>
2025-10-02 11:27:27 -0300

    Implementa layout da tela de limpeza terminal

---
+++ ProjetoHospitalShared/ViewModels/LimpezaTerminalViewModel.cs
@@ -0,0 +1,89 @@
+﻿namespace ProjetoHospitalShared.ViewModels
+{
+    public class LimpezaTerminalViewModel
+    {
+        public LimpezaTerminalViewModel()
+        {
+        }
+
+        public LimpezaTerminalViewModel(
+            int leitoId,
+            int funcionarioId,
+            DateTime dataHoraFim,
+            bool tirarLixo,
+            bool vasoSanitario,
+            bool limparBox,
+            bool revisarMofo,
+            bool limparPia,
+            bool limparCama,
+            bool limparEscadaCama,
+            bool limparMesaCabeceira,
+            bool limparArmario,
+            bool recolherRoupaSuja,
+            bool revisarPapelToalhaEHigienico,
+            bool limparDispensers,
+            bool limparLixeira,
+            bool limparTeto,
+            bool limparParedes,
+            bool limparChao)
+        {
+            this.LeitoId = leitoId;
+            this.FuncionarioId = funcionarioId;
+            this.DataHoraFim = dataHoraFim;
+            this.TirarLixo = tirarLixo;
+            this.VasoSanitario = vasoSanitario;
+            this.LimparBox = limparBox;
+            this.RevisarMofo = revisarMofo;
+            this.LimparPia = limparPia;
+            this.LimparCama = limparCama;
+            this.LimparEscadaCama = limparEscadaCama;
+            this.LimparMesaCabeceira = limparMesaCabeceira;
+            this.LimparArmario = limparArmario;
+            this.RecolherRoupaSuja = recolherRoupaSuja;
+            this.RevisarPapelToalhaEHigienico = revisarPapelToalhaEHigienico;
+            this.LimparDispensers = limparDispensers;
+            this.LimparLixeira = limparLixeira;
+            this.LimparTeto = limparTeto;
+            this.LimparParedes = limparParedes;
+            this.LimparChao = limparChao;
+        }
+
+        public int LeitoId { get; set; }
+
+        public int FuncionarioId { get; set; }
+
+        public DateTime DataHoraFim { get; set; }
+
+        public bool TirarLixo { get; set; }
+
+        public bool VasoSanitario { get; set; }
+
+        public bool LimparBox { get; set; }
+
+        public bool RevisarMofo { get; set; }
+
+        public bool LimparPia { get; set; }
+
+        public bool LimparCama { get; set; }
+
+        public bool LimparEscadaCama { get; set; }
+
+        public bool LimparMesaCabeceira { get; set; }
+
+        public bool LimparArmario { get; set; }
+
+        public bool RecolherRoupaSuja { get; set; }
+
+        public bool RevisarPapelToalhaEHigienico { get; set; }
+
+        public bool LimparDispensers { get; set; }
+
+        public bool LimparLixeira { get; set; }
+
+        public bool LimparTeto { get; set; }
+
+        public bool LimparParedes { get; set; }
+
+        public bool LimparChao { get; set; }
+    }
+}
---
+++ ProjetoHospitalWebAssembly/Pages/LimpezaTerminal.razor.cs
@@ -0,0 +1,58 @@
+﻿namespace ProjetoHospitalWebAssembly.Pages
+{
+    using Microsoft.AspNetCore.Components;
+    using ProjetoHospitalShared.ViewModels;
+
+    public partial class LimpezaTerminal : ComponentBase
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string IdLeito { get; set; } = string.Empty;
+
+        private bool isLoading = false;
+
+        private LimpezaTerminalViewModel limpeza = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            this.isLoading = true;
+            this.StateHasChanged();
+
+            this.limpeza.LeitoId = int.Parse(this.IdLeito);
+
+            this.isLoading = false;
+            this.StateHasChanged();
+        }
+
+        private void MarcarTodosComoRealizado()
+        {
+            this.limpeza.TirarLixo = true;
+            this.limpeza.VasoSanitario = true;
+            this.limpeza.LimparBox = true;
+            this.limpeza.RevisarMofo = true;
+            this.limpeza.LimparPia = true;
+            this.limpeza.LimparCama = true;
+            this.limpeza.LimparEscadaCama = true;
+            this.limpeza.LimparMesaCabeceira = true;
+            this.limpeza.LimparArmario = true;
+            this.limpeza.RecolherRoupaSuja = true;
+            this.limpeza.RevisarPapelToalhaEHigienico = true;
+            this.limpeza.LimparDispensers = true;
+            this.limpeza.LimparLixeira = true;
+            this.limpeza.LimparTeto = true;
+            this.limpeza.LimparParedes = true;
+            this.limpeza.LimparChao = true;
+        }
+
+        private async Task FinalizarLimpezaAsync()
+        {
+            this.limpeza.DataHoraFim = DateTime.Now;
+
+            // TODO: Implementar a lógica de finalização da limpeza terminal
+            this.NavigationManager
+                .NavigateTo($"/quartos-para-limpar");
+        }
+    }
+}
